c7e0357638aa963a10098649efcdd2f4ae118295
Danijel Dupor <danijeldupor.dd@gmail.com>
2021-04-28 16:43:22 +0200

    BACKEND_commit1

--- src/index.js
+++ src/index.js
@@ -13,19 +13,62 @@ app.get('/', (req, res) => {
     res.json({});
 });
 // Za zaprimanje rezervacije parkirnog mjesta sa frontenda.
-app.post('/rezervacija_parkinga', (req, res) => {
+app.post('/osobni_podaci', (req, res) => {
     let doc= req.body;
 
-    storage.rezervacije_parkinga.push(doc);
+    storage.osobni_podaci.push(doc);
 
     
     res.json({status: 'OK'});
 });
 
-// Za listanje svih rezervacija parkirnih mjesta iz administracije.
-app.get('/rezervacija_parkinga', (req, res) => {
+app.post('/podaci_vozila', (req, res) => {
+    let doc= req.body;
+
+    storage.podaci_vozila.push(doc);
+
     
-    res.json(storage.rezervacije_parkinga);
+    res.json({status: 'OK'});
+});
+
+app.post('/podaci_rezervacije', (req, res) => {
+    let doc= req.body;
+
+    storage.podaci_rezervacije.push(doc);
+
+    
+    res.json({status: 'OK'});
+});
+
+app.post('/kalkulator_cijene_parkinga', (req, res) => {
+    let doc= req.body;
+
+    storage.kalkulator_cijene_parkinga.push(doc);
+
+    
+    res.json({status: 'OK'});
+});
+
+
+// Za listanje svih rezervacija parkirnih mjesta iz administracije.
+app.get('/osobni_podaci', (req, res) => {
+    
+    res.json(storage.osobni_podaci);
+});
+
+app.get('/podaci_vozila', (req, res) => {
+    
+    res.json(storage.podaci_vozila);
+});
+
+app.get('/podaci_rezervacije', (req, res) => {
+    
+    res.json(storage.podaci_rezervacije);
+});
+
+app.get('/kalkulator_cijene_parkinga', (req, res) => {
+    
+    res.json(storage.kalkulator_cijene_parkinga);
 });
 
 
