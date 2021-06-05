b9a62c11f3f94709b9a6ab96598bff50848d3f09
Danijel Dupor <danijeldupor.dd@gmail.com>
2021-06-05 17:20:41 +0200

    BACKEND_commit3

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import storage from './memory_storage.js';
 import cors from 'cors';
+import connect from './db.js'
+
 
 const app = express(); // instanciranje aplikacije
 const port = 3200; // port na kojem će web server slušati
+
 
 app.use(cors());
 app.use(express.json());
@@ -51,22 +54,31 @@ app.post('/kalkulator_cijene_parkinga', (req, res) => {
 
 
 // Za listanje svih rezervacija parkirnih mjesta iz administracije.
-app.get('/osobni_podaci', (req, res) => {
+
+app.get('/osobni_podaci', async (req, res)=> {
+    let db= await connect()
+    let cursor = await db.collection("Parkfinder").find().sort({id_korisnika})
+    let results = await cursor.toArray()
+    res.json(results)
+    console.log(results)
+})
+
+app.get('/osobni_podaci_memory', (req, res) => {
     
     res.json(storage.osobni_podaci);
 });
 
-app.get('/podaci_vozila', (req, res) => {
+app.get('/podaci_vozila_memory', (req, res) => {
     
     res.json(storage.podaci_vozila);
 });
 
-app.get('/podaci_rezervacije', (req, res) => {
+app.get('/podaci_rezervacije_memory', (req, res) => {
     
     res.json(storage.podaci_rezervacije);
 });
 
-app.get('/kalkulator_cijene_parkinga', (req, res) => {
+app.get('/kalkulator_cijene_parkinga_memory', (req, res) => {
     
     res.json(storage.kalkulator_cijene_parkinga);
 });
